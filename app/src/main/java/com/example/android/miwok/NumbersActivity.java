81a7cf815a6117878dda6b597396a6aa12c3317b
Sopholez <jdhoetzel@gmail.com>
2018-09-19 13:31:22 -0600

    Added a for loop to create text views in numbers activity

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,6 +17,11 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -24,5 +29,28 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        // Create and array list of the english numbers
+        ArrayList<String> numbersEnglish = new ArrayList<String>();
+        numbersEnglish.add("One");
+        numbersEnglish.add("Two");
+        numbersEnglish.add("Three");
+        numbersEnglish.add("Four");
+        numbersEnglish.add("Five");
+        numbersEnglish.add("Six");
+        numbersEnglish.add("Seven");
+        numbersEnglish.add("Eight");
+        numbersEnglish.add("Nine");
+        numbersEnglish.add("Ten");
+
+        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+
+        // Creates a text view for each word in the numbersEnglish arrayList
+        for (int i = 0; i < numbersEnglish.size(); i++) {
+            TextView txt = new TextView(this);
+            txt.setId(i);
+            txt.setText(numbersEnglish.get(i));
+            rootView.addView(txt);
+        }
     }
 }
